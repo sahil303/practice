ef3f319658d9ba1384848d032cfad92f61ac9cf3
Sahil303 <sahiltamboli303@gmail.com>
2021-09-18 23:01:31 +0530

    Fries

--- Hungary.py
+++ Hungary.py
@@ -3,6 +3,7 @@ hungry = str(input("Are you hungary?"))
 if hungry == 'yes':
     print("Eat Samosa")
     print("Eat Pizza")
-    print("Eat Burger") 
+    print("Eat Burger")
+    print("Eat Fries")
 else:
     print ("Do Your Homework")
