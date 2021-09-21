532fab76cba71e33b0cc439a43987d1439fc3871
Sahil303 <sahiltamboli303@gmail.com>
2021-09-21 19:52:51 +0530

    Sum of even and odd numbers

--- evenodd.py
+++ evenodd.py
@@ -1,6 +1,19 @@
 num = int(input("Enter number:: "))
-
+flag = 0
 if num % 2== 0:
+    flag = 1
     print("\nEven Number")
 else:
+    
     print("\nOdd Number")
+    
+if flag == 1:
+    n = num
+    sum = (n*(n+1))/2
+    print("Sum of Even numbers upto ",num," is :",sum)
+else:
+    n = num
+    sum = n**2
+    print("Sum of Odd numbers upto ",num," is :",sum)
+
+print("Thankyou")
